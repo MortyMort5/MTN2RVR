710ae67cf920f25a2ac0da74fc396da33555a723
Morty <mortmort5@gmail.com>
2017-07-18 17:27:54 -0600

    Initial Commit

---
+++ MTN2RVRTests/MTN2RVRTests.swift
@@ -0,0 +1,36 @@
+//
+//  MTN2RVRTests.swift
+//  MTN2RVRTests
+//
+//  Created by Sterling Mortensen on 7/18/17.
+//  Copyright © 2017 Git Swifty. All rights reserved.
+//
+
+import XCTest
+@testable import MTN2RVR
+
+class MTN2RVRTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
